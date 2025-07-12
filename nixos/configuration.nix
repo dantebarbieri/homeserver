969cc9096edcf4e59510267396124fe1ed95aac4
dantebarbieri <dantevbarbieri@gmail.com>
2025-07-12 15:54:20 -0500

    wip: configuration.nix
    TODO:
    - zim/oh-my-zsh
    - docker stuff
    - git stuff
    - nvidia stuff
    - ddclient (dynamic dns)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -100,7 +100,6 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    zsh
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
